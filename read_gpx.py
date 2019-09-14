943d4d52c7cb36498062db690f5d5f35c3f11b41
Martin Korinek <work@korinek.cc>
2019-09-14 22:15:33 +0200

    Read multiple files and print them in png

--- read_gpx.py
+++ read_gpx.py
@@ -1,5 +1,35 @@
-import gpxpy
+from os import listdir
+from os.path import isfile, join
 import matplotlib.pyplot as plt
+import gpxpy
 
-gpx_file = open('../tracks/track_2019-09-12_07-15.gpx', 'r')
-gpx = gpxpy.parse(gpx_file)
+data_path = '../tracks'
+data = [f for f in listdir(data_path) if isfile(join(data_path,f))]
+
+lat = []
+lon = []
+
+fig = plt.figure(facecolor = '0.05')
+ax = plt.Axes(fig, [0., 0., 1., 1.], )
+ax.set_aspect('equal')
+ax.set_axis_off()
+fig.add_axes(ax)
+
+for activity in data:
+    gpx_filename = join(data_path,activity)
+    gpx_file = open(gpx_filename, 'r')
+    print("Opend:", gpx_filename)
+    gpx = gpxpy.parse(gpx_file)
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                lat.append(point.latitude)
+                lon.append(point.longitude)
+    plt.plot(lon, lat, color = 'deepskyblue', lw = 0.2, alpha = 0.8)
+    lat = []
+    lon = []
+
+filename = data_path + '/track.png'
+plt.savefig(filename, facecolor = fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=300)
+print("Stored file:", filename)
